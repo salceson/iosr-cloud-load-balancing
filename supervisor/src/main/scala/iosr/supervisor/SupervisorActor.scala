9142767bf07e95632f288be57397144e9f018a88
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2016-12-19 17:40:32 +0100

    Supervisor fix

--- supervisor/src/main/scala/iosr/supervisor/SupervisorActor.scala
+++ supervisor/src/main/scala/iosr/supervisor/SupervisorActor.scala
@@ -6,18 +6,18 @@ import iosr.Messages._
 
 class SupervisorActor extends Actor with ActorLogging {
 
-  private val router = Router(RoundRobinRoutingLogic())
+  private var router = Router(RoundRobinRoutingLogic())
 
   override def receive: Receive = {
     case RegisterWorker =>
       val worker = sender()
       log.info(s"Registering worker $worker")
-      router.addRoutee(worker)
+      router = router.addRoutee(worker)
       worker ! RegisterWorkerAck
     case DeregisterWorker =>
       val worker = sender()
       log.info(s"Deregistering worker $worker")
-      router.removeRoutee(worker)
+      router = router.removeRoutee(worker)
       worker ! DeregisterWorkerAck
     case msg: Request =>
       router.route(msg, sender())
